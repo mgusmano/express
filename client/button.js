5394e5987fd092f2225484c5a4612e246847388b
Marc Gusmano <mfusmano@yahoo.com>
2022-02-19 12:11:24 -0500

    json

--- client/button.js
+++ client/button.js
@@ -1,6 +1,7 @@
 const getJson = async () => {
     try {
-        const response = await axios.get('http://localhost:3000/json?a=1&b=2');
+        //const response = await axios.get('http://localhost:3000/json?a=1&b=2');
+        const response = await axios.get('https://mjgnode.herokuapp.com/json');
         const items = response.data;
         return items;
     } catch (errors) {
